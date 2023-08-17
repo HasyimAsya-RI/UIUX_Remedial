9cda28b10411e0e300f1bec1fc315c5592ad105c
Hasyim Asy'ari <hasyimasya_ri@yahoo.com>
2023-08-17 20:57:32 +0700

    Create ClassActivity.kt

---
+++ app/src/main/java/com/hasyimasya_ri/si138remidi/home/ClassActivity.kt
@@ -0,0 +1,12 @@
+package com.hasyimasya_ri.si138remidi.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hasyimasya_ri.si138remidi.R
+
+class ClassActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_class)
+    }
+}
